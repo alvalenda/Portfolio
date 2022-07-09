89b6782a72cdc03822f7390c6f48c9277eb5801c
alvalenda <banysan@msn.com>
2022-07-08 21:00:28 -0300

    +function main +function iniciarEventos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-function anchorClique() {
+function homeLogoClique() {
     document.querySelector('#anchor-logo').addEventListener('click', () => {
         digitarPorLetras(document.querySelector('.home-digitado'));
     });
@@ -26,30 +26,39 @@ function digitarPorLetras(elemento) {
     });
 }
 
-digitarPorLetras(document.querySelector('.home-digitado'));
-anchorClique();
+function iniciarEventos() {
+    // Inicia Eventos de Mouse Hover das Cards de Skills
+    hoverMudaDescricao(
+        '.html',
+        'HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir.',
+    );
+    hoverMudaDescricao(
+        '.css',
+        'CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML.',
+    );
+    hoverMudaDescricao(
+        '.js',
+        'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e, funcional).',
+    );
+    hoverMudaDescricao(
+        '.py',
+        'Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.',
+    );
+    hoverMudaDescricao(
+        '.nodejs',
+        'Node.js é um software de código aberto, multiplataforma, baseado no interpretador V8 do Google e que permite a execução de códigos JavaScript fora de um navegador web.',
+    );
+    hoverMudaDescricao(
+        '.c',
+        'C é uma linguagem de programação compilada de propósito geral, estruturada, imperativa, procedural, padronizada pela Organização Internacional para Padronização (ISO).',
+    );
 
-hoverMudaDescricao(
-    '.html',
-    'HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir.',
-);
-hoverMudaDescricao(
-    '.css',
-    'CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML.',
-);
-hoverMudaDescricao(
-    '.js',
-    'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e, funcional).',
-);
-hoverMudaDescricao(
-    '.py',
-    'Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.',
-);
-hoverMudaDescricao(
-    '.nodejs',
-    'Node.js é um software de código aberto, multiplataforma, baseado no interpretador V8 do Google e que permite a execução de códigos JavaScript fora de um navegador web.',
-);
-hoverMudaDescricao(
-    '.c',
-    'C é uma linguagem de programação compilada de propósito geral, estruturada, imperativa, procedural, padronizada pela Organização Internacional para Padronização (ISO).',
-);
+    // Inicia Evento de clicar na Logo e Reescrever texto digitado na HOME
+    homeLogoClique();
+}
+
+function main() {
+    digitarPorLetras(document.querySelector('.home-digitado'));
+    iniciarEventos();
+}
+main();
