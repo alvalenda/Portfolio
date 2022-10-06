c86a7edf4f03c1cfa668d5cb28006bb76b14a3c5
alvalenda <banysan@msn.com>
2022-10-06 17:08:41 -0300

    setup: jsconfig, vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import reactImage from "@assets/react.svg";
 
 function App() {
-  return <div className="App">Olá Mundo!</div>;
+  return (
+    <>
+      <div className="App">Olá Mundo!</div>
+      <img src={reactImage} alt="" />
+    </>
+  );
 }
 
 export default App;
