63bec2759dd2efb3e950f8b645c562d43f22fafa
alvalenda <banysan@msn.com>
2022-10-07 15:35:45 -0300

    feat: header components

---
+++ src/components/layout/Header/HeaderSocials.jsx
@@ -0,0 +1,29 @@
+import { BsLinkedin, BsGithub, BsDiscord } from "react-icons/bs";
+
+export const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/flavio-alvarenga/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://github.com/alvalenda"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsGithub />
+      </a>
+      <a
+        href="https://discordapp.com/users/246040430494351362"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsDiscord />
+      </a>
+    </div>
+  );
+};
--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,5 +1,25 @@
 import "./Header.css";
+import "./CTA";
+import { CTA } from "./CTA";
+import { HeaderSocials } from "./HeaderSocials";
+import ME from "@assets/me.png";
 
 export const Header = () => {
-  return <nav>Header</nav>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Heelo I'm</h5>
+        <h1>Flávio Alvarenga</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="picture of me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
