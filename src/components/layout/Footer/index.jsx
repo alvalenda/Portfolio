b51f21f4c345822ac82780607a03e2490d3332c3
alvalenda <banysan@msn.com>
2022-10-12 09:38:33 -0300

    feat: Footer style

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <footer id="#footer">
+    <footer id="footer">
       <a href="#" className="footer__logo">
         ALVA
       </a>
