e8aec1e487f2b4886b25bd5ad292916d795f8957
alvalenda <banysan@msn.com>
2022-10-13 16:51:50 -0300

    feat: utils -> testimonials.js

---
+++ src/utils/testimonials.js
@@ -0,0 +1,31 @@
+import AVT1 from "@assets/avatar1.jpg";
+import AVT2 from "@assets/avatar2.jpg";
+import AVT3 from "@assets/avatar3.jpg";
+import AVT4 from "@assets/avatar4.jpg";
+
+export const testimonialsData = [
+  {
+    name: "Person One",
+    image: AVT1,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eaque maiores adipisci reprehenderit esse exercitationem nam, ipsam est labore. Tempore soluta fugit ipsam perspiciatis? Tempora culpa dolor iusto optio laborum!",
+  },
+  {
+    name: "Person Two",
+    image: AVT2,
+    review:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla inventore placeat voluptas impedit magni fugit quasi voluptates unde voluptatibus obcaecati perferendis dicta corporis aspernatur qui iste cupiditate, illum quisquam facilis.",
+  },
+  {
+    name: "Person Three",
+    image: AVT3,
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat deserunt, eius quis at facilis doloribus, voluptatem sit sunt distinctio debitis sapiente consectetur officia voluptatibus enim officiis minus sint quia odit?",
+  },
+  {
+    name: "Person Four",
+    image: AVT4,
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolorum provident rem vero consequatur possimus eaque distinctio architecto accusantium nulla dolore, minima necessitatibus, similique quos! Voluptatum, maxime iure? Asperiores, quia!",
+  },
+];
