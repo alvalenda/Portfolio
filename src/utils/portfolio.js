e6bb314c01843152b36861527239a22c58d163aa
alvalenda <banysan@msn.com>
2022-10-13 16:28:33 -0300

    rename const

---
+++ src/utils/portfolio.js
@@ -0,0 +1,45 @@
+import IMG1 from "@assets/portfolio1.jpg";
+import IMG2 from "@assets/portfolio2.jpg";
+import IMG3 from "@assets/portfolio3.jpg";
+import IMG4 from "@assets/portfolio4.jpg";
+import IMG5 from "@assets/portfolio5.png";
+import IMG6 from "@assets/portfolio6.jpg";
+
+export const portfolioData = [
+  {
+    image: IMG1,
+    title: "Title of Project 1",
+    github: "https://github.com/alvalenda/project",
+    demo: "https://liveversionoftheproject.com",
+  },
+  {
+    image: IMG2,
+    title: "Title of Project 2",
+    github: "https://github.com/alvalenda/project",
+    demo: "https://liveversionoftheproject.com",
+  },
+  {
+    image: IMG3,
+    title: "Title of Project 3",
+    github: "https://github.com/alvalenda/project",
+    demo: "https://liveversionoftheproject.com",
+  },
+  {
+    image: IMG4,
+    title: "Title of Project 4",
+    github: "https://github.com/alvalenda/project",
+    demo: "https://liveversionoftheproject.com",
+  },
+  {
+    image: IMG5,
+    title: "Title of Project 5",
+    github: "https://github.com/alvalenda/project",
+    demo: "https://liveversionoftheproject.com",
+  },
+  {
+    image: IMG6,
+    title: "Title of Project 6",
+    github: "https://github.com/alvalenda/project",
+    demo: "https://liveversionoftheproject.com",
+  },
+];
