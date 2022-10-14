9e85c1c0d16b815588e813bd46bd9dec0f09e339
alvalenda <banysan@msn.com>
2022-10-13 22:22:36 -0300

    motion

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -38,7 +38,7 @@ export const boxHello = {
   visible: {
     x: 0,
     opacity: 1,
-    scale: [0, 1],
+    scale: [0, 1.25, 1],
     transition: {
       duration: 1.2,
     },
@@ -61,7 +61,7 @@ export const boxName = {
     },
   },
   hidden: {
-    x: -200,
+    x: 200,
     opacity: 0,
     scale: 0,
   },
@@ -69,10 +69,10 @@ export const boxName = {
 
 export const boxJob = {
   visible: {
-    y: [-150, -100, -50, 0, -50, 0],
+    y: 0,
     opacity: 1,
     rotate: [0, 1080],
-    scale: [0, 1.5, 1],
+    scale: [0, 1],
     transition: {
       duration: 1.2,
       delay: 2.0,
