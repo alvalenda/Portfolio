2045f011d46d205a9103db229c9d4fd9d1ab81eb
alvalenda <banysan@msn.com>
2022-10-17 13:56:59 -0300

    feat: New Portfolio card style

--- src/components/Portfolio/ProjectCard.jsx
+++ src/components/Portfolio/ProjectCard.jsx
@@ -2,6 +2,9 @@ import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 import { boxFromLeft, boxFromRight } from "@/utils/motion";
+import { AiOutlineFolder } from "react-icons/ai";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { FiGithub } from "react-icons/fi";
 
 export const ProjectCard = ({ project, number }) => {
   const control = useAnimation();
@@ -24,31 +27,68 @@ export const ProjectCard = ({ project, number }) => {
       animate={control}
     >
       <article className="portfolio__item">
-        <div className="portfolio__item-image">
-          <img src={project.image} alt={`image of ${project.title}`} />
-        </div>
-        <h3>{project.title}</h3>
-
         <div className="portfolio__item-cta">
-          <a
-            href={project.github}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn"
-          >
-            Github
-          </a>
+          <AiOutlineFolder id="portfolio__item-folder" />
 
-          <a
-            href={project.demo}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-primary"
-          >
-            Deploy
-          </a>
+          <div>
+            <a
+              href={project.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              // className="btn"
+            >
+              <FiGithub />
+            </a>
+
+            <a
+              href={project.demo}
+              target="_blank"
+              rel="noopener noreferrer"
+              // className="btn btn-primary"
+            >
+              <HiOutlineExternalLink />
+            </a>
+          </div>
+        </div>
+
+        <h3 className="portfolio__item-title">{project.title}</h3>
+        <p className="portfolio__item-description">{project.description}</p>
+
+        <div className="portfolio__item-tech">
+          {project.tech.map((tech, index) => (
+            <span key={index}>{tech}</span>
+          ))}
         </div>
       </article>
     </motion.div>
   );
 };
+
+{
+  /* <article className="portfolio__item">
+<div className="portfolio__item-image">
+  <img src={project.image} alt={`image of ${project.title}`} />
+</div>
+<h3>{project.title}</h3>
+
+<div className="portfolio__item-cta">
+  <a
+    href={project.github}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="btn"
+  >
+    Github
+  </a>
+
+  <a
+    href={project.demo}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="btn btn-primary"
+  >
+    Deploy
+  </a>
+</div>
+</article> */
+}
