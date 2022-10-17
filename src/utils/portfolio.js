f59a9b135eff390d9c523e35721cff41acf1a6b5
alvalenda <banysan@msn.com>
2022-10-17 20:18:15 -0300

    adding projects

--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -1,22 +1,13 @@
-import IMG1 from "@assets/portfolio1.jpg";
-import IMG2 from "@assets/portfolio2.jpg";
-import IMG3 from "@assets/portfolio3.jpg";
-import IMG4 from "@assets/portfolio4.jpg";
-import IMG5 from "@assets/portfolio5.png";
-import IMG6 from "@assets/portfolio6.jpg";
-
 export const portfolioData = [
   {
-    image: IMG1,
-    title: "Title of Project 1",
+    title: "Todo List React App",
     description:
       "Description of Project 1 lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-    github: "https://github.com/alvalenda/project",
-    demo: "https://liveversionoftheproject.com",
-    tech: ["React", "Node", "MongoDB"],
+    github: "https://github.com/alvalenda/front-todolist",
+    demo: "https://alvalenda-todo.netlify.app/",
+    tech: ["React", "JavaScript", "css"],
   },
   {
-    image: IMG2,
     title: "Title of Project 2",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Description of Project 2",
@@ -25,7 +16,6 @@ export const portfolioData = [
     tech: ["React", "Node", "MongoDB"],
   },
   {
-    image: IMG3,
     title: "Title of Project 3",
     description:
       "Description of Project 3 lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
@@ -34,7 +24,6 @@ export const portfolioData = [
     tech: ["React", "Node", "MongoDB"],
   },
   {
-    image: IMG4,
     title: "Title of Project 4",
     description: "Description of Project 4",
     github: "https://github.com/alvalenda/project",
@@ -42,7 +31,6 @@ export const portfolioData = [
     tech: ["React", "Node", "MongoDB"],
   },
   {
-    image: IMG5,
     title: "Title of Project 5",
     description: "Description of Project 5",
     github: "https://github.com/alvalenda/project",
@@ -50,7 +38,6 @@ export const portfolioData = [
     tech: ["React", "Node", "MongoDB"],
   },
   {
-    image: IMG6,
     title: "Title of Project 6",
     description: "Description of Project 6",
     github: "https://github.com/alvalenda/project",
