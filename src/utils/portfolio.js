00b7da107e8e7aebc48534a1936bfd6fd3896c20
alvalenda <banysan@msn.com>
2022-10-18 17:52:25 -0300

    feat: my projects

--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -1,47 +1,66 @@
 export const portfolioData = [
   {
-    title: "Todo List React App",
+    title: "To-Do List App",
     description:
-      "Description of Project 1 lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+      "A simple to-do list applying Optimistic UI pattern. v1 consuming a backend and v2 storing in localStorage.",
     github: "https://github.com/alvalenda/front-todolist",
     demo: "https://alvalenda-todo.netlify.app/",
-    tech: ["React", "JavaScript", "css"],
+    tech: ["React", "Javascript", "css"],
   },
   {
-    title: "Title of Project 2",
+    title: "Github Finder",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Description of Project 2",
-    github: "https://github.com/alvalenda/project",
-    demo: "https://liveversionoftheproject.com",
-    tech: ["React", "Node", "MongoDB"],
+      "A react client that consumes the github api to search for users and their repositories. ",
+    github: "https://github.com/alvalenda/github-finder",
+    demo: "http://github-finder-alvalenda.vercel.app/",
+    tech: ["React", "Tailwind", "Javascript"],
   },
   {
-    title: "Title of Project 3",
+    title: "API Brasileirão v1",
     description:
-      "Description of Project 3 lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-    github: "https://github.com/alvalenda/project",
-    demo: "https://liveversionoftheproject.com",
-    tech: ["React", "Node", "MongoDB"],
+      "First version of the Brazilian football league API. It includes the 20 teams and their respective matches and datas.",
+    github: "https://github.com/alvalenda/api-brasileirao/tree/mongodb-version",
+    demo: "https://github.com/alvalenda/api-brasileirao/",
+    tech: ["Nodejs", "express", "MongoDB"],
   },
   {
-    title: "Title of Project 4",
-    description: "Description of Project 4",
-    github: "https://github.com/alvalenda/project",
-    demo: "https://liveversionoftheproject.com",
-    tech: ["React", "Node", "MongoDB"],
+    title: "Rick and Morty API",
+    description:
+      "A CRUD for Rick and Morty characters, with a login system and cryptographed password. JWT and Swagger applied.",
+    github: "https://github.com/alvalenda/api-rickandmorty",
+    demo: "https://alva-rickandmorty.herokuapp.com/api-docs/",
+    tech: ["Nodejs", "Express", "MongoDB"],
   },
   {
-    title: "Title of Project 5",
-    description: "Description of Project 5",
-    github: "https://github.com/alvalenda/project",
-    demo: "https://liveversionoftheproject.com",
-    tech: ["React", "Node", "MongoDB"],
+    title: "To-Do List API",
+    description:
+      "A simple CRUD API for interacting with MongoDB and the To-Do List App. Developed in NodeJS and Express.",
+    github: "https://github.com/alvalenda/api-todolist",
+    demo: "https://api-todolist-alvalenda.herokuapp.com/api-docs/",
+    tech: ["Nodejs", "express", "MongoDB"],
   },
   {
-    title: "Title of Project 6",
-    description: "Description of Project 6",
-    github: "https://github.com/alvalenda/project",
-    demo: "https://liveversionoftheproject.com",
-    tech: ["React", "Node", "MongoDB"],
+    title: "Stopwatch",
+    description:
+      "An alarm and stopwatch for monitoring time. Data persistence in localStorage.",
+    github: "https://github.com/alvalenda/stopwatch",
+    demo: "https://alvalenda.github.io/stopwatch/",
+    tech: ["Javascript", "HTML", "css"],
+  },
+  {
+    title: "Pokematch",
+    description:
+      "A simple game where you have to remember the pokemon's position. It consumes the pokeapi.",
+    github: "https://github.com/alvalenda/pokematch",
+    demo: "https://alvalenda.github.io/pokematch/",
+    tech: ["Javascript", "HTML", "css"],
+  },
+  {
+    title: "Fighting Game",
+    description:
+      "A simple fighting game where you have to defeat your opponent. Created with Javascript canvas.",
+    github: "https://github.com/alvalenda/jogo-de-luta",
+    demo: "https://alvalenda-jogo-de-luta.netlify.app/",
+    tech: ["Javascript", "canvas", "oop"],
   },
 ];
