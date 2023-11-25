9a3283cd22151d09cc1fa6996e47fde2c784f17b
alvalenda <banysan@msn.com>
2023-11-25 08:14:51 -0300

    Update about.js with new job title

--- src/utils/about.js
+++ src/utils/about.js
@@ -2,7 +2,6 @@ export const aboutStrings = [
   "I don't drink coffee to wake up.",
   " I wake up to drink coffee.",
   "< And I drink coffee to code. />",
-  "Tech geek, fantasy lover, and a passionate developer.",
-  "Computer Systems Analyst | Fullstack Developer",
+  "Computer Systems Analyst & Fullstack Developer",
   "Let's talk about your project!",
 ];
